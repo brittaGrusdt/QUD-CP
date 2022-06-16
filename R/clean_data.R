9c43535c47c520fd8a04b0910ed6ceabe00c357a
Britta Grusdt <bgrusdt@gmail.com>
2022-06-16 10:30:53 +0200

    added results, preregistration and analysis code

---
+++ R/clean_data.R
@@ -0,0 +1,112 @@
+library(here)
+source(here("R", "utils.R"))
+main_data <- read_csv(here("results", "results_67_communicationBlocks_BG.csv"))
+pilot_data <- read_csv(here("results", "results_67_communicationBlocks_BG_pilot.csv"))
+
+# Clean Data --------------------------------------------------------------
+
+# join original pilot data and
+# remove data from our test-runs if had not been done before
+data.original = bind_rows(pilot_data, main_data) %>% 
+  filter(!str_detect(prolific_id, "test-")) %>% 
+  mutate(question = case_when(question == "if_ant" ~ "ifp", 
+                              question == "cons" ~ "willq",
+                              T ~ question))
+# clean according to predefined exclusion criteria
+data.cleaned = clean_data(data.original)
+
+# sanity check whether there are duplicate prolific ids (according to settings
+# in prolific this should normally not be the case)
+duplicate_prolific_ids = data.cleaned %>% 
+  dplyr::group_by(submission_id, prolific_id, startDate) %>% 
+  dplyr::count() %>%
+  group_by(prolific_id) %>% dplyr::count() %>% filter(n > 1) %>% 
+  pull(prolific_id)
+
+message("prolific_ids that were not unique (despite exclusion criteria defined in Prolific):")
+message(paste(duplicate_prolific_ids, collapse=", "))
+
+# remove later data from duplicate prolific_ids:
+fst_batch = "Sat Oct 30 2021"
+snd_batch = "Mon Nov 15 2021"
+pilot_batch = "Thu Sep 23 2021"
+
+duplicates.out = data.original %>% 
+  filter(prolific_id %in% duplicate_prolific_ids) %>% 
+  dplyr::select(submission_id, prolific_id, startDate) %>% 
+  distinct() %>%
+  filter(str_detect(startDate, snd_batch))
+
+data = anti_join(data.cleaned, duplicates.out)
+
+# check for comments
+comments = data %>% select(comments, submission_id) %>% 
+  filter(!is.na(comments) & comments!="") %>% distinct()
+
+comments$comments
+comments[11,]$comments
+comments[24,]$comments
+comments[30,]$comments
+
+out.comments = comments[c(11, 24), ] %>% dplyr::select(submission_id)
+
+data = anti_join(data, out.comments)
+
+n_all = data.original$submission_id %>% unique() %>% length
+n_kept = data$submission_id %>% unique() %>% length
+ratio_kept = round(n_kept/n_all, 4)
+msg <- "% of all recorded data after cleaning that is included in analysis:"
+message(paste(msg, ratio_kept))
+write_csv(data, here("results", "data_cleaned.csv"))
+
+
+# numbers removed data ----------------------------------------------------
+data.controls <- data.original %>%
+  group_by(submission_id) %>% 
+  filter(startsWith(type, "control-") | type == "test-example") %>% 
+  mutate(correct = expected == selected_pic) %>% 
+  dplyr::select(correct, type, submission_id, prolific_id, id, timeSpent,
+                comments, startDate) %>% 
+  filter(!str_detect(prolific_id, "test-"))
+
+# 1. Wrong answer in test example
+out.test_ex = data.controls %>% filter(type == "test-example" & !correct) %>%
+  dplyr::select(submission_id, startDate, comments)
+
+# 2. more than 1 control-trial wrong
+out.control = data.controls %>% filter(type == "control-physics") %>% 
+  group_by(submission_id, prolific_id, timeSpent, comments, startDate) %>% 
+  summarize(nb_correct = sum(correct), .groups = "drop_last") %>%
+  filter(nb_correct < 2)
+
+# 3. failed attention-checks
+attention_checks = data.controls %>% filter(type=="control-random")
+out.attention = attention_checks %>%
+  group_by(submission_id, prolific_id, timeSpent, comments, startDate) %>% 
+  summarize(nb_correct = sum(correct), .groups = "drop_last") %>%
+  filter(nb_correct != 3)
+
+# 4. total number of participants removed
+out.all = c(out.test_ex$submission_id, out.control$submission_id, 
+            out.attention$submission_id, out.comments$submission_id,
+            duplicates.out$submission_id) %>% unique() 
+out.n = out.all %>% length()
+
+data.keep = anti_join(
+  data.original %>% dplyr::select(submission_id, prolific_id) %>% distinct(),
+  tibble(submission_id=out.all)
+)
+# Print numbers
+message(paste("#submissions with wrong test-example:", 
+              out.test_ex$submission_id %>% unique() %>% length()))
+message(paste("#submissions with more than 1 wrong control trial:", 
+              out.control$submission_id %>% unique() %>% length()))
+message(paste("#submissions with at least one wrong attention-check trial:", 
+              out.attention$submission_id %>% unique() %>% length()))
+message(paste("#submissions removed due to several recordings:", 
+              duplicates.out$submission_id %>% unique() %>% length()))
+message(paste("#submissions removed due to comments:", 
+              out.comments$submission_id %>% unique() %>% length()))
+
+message(paste("#submissions that were excluded for the analysis:", out.n))
+
---
+++ R/utils.R
@@ -0,0 +1,110 @@
+library(boot)
+library(here)
+
+# at most one control-physics trial and none of the control-random trials wrong
+# and test-example trial must be correctly replied to
+clean_data = function(data){
+  controls = data %>% filter(startsWith(type, "control") | type=="test-example") %>% 
+    dplyr::select(c(submission_id, starts_with("id"), type, question,
+                    selected_pic, expected)) %>% 
+    group_by(submission_id, type) %>% 
+    mutate(n=n()) %>% 
+    mutate(correct = selected_pic == expected, n_correct = sum(correct)) %>% 
+    distinct_at(vars(c(type, submission_id)), .keep_all = T)
+  
+  submission_ids.out = controls %>% 
+    filter(!(type == "control-random" & n_correct == n) &
+           !(type == "control-physics" & n_correct >= n-1) &
+           !(type == "test-example" & n_correct == 1)) %>% 
+    pull(submission_id) %>% unique()
+  print(paste('remove data from participants:',
+              paste(submission_ids.out, collapse = ", ")))
+  return(data %>% filter(!submission_id %in% submission_ids.out))
+}
+
+data_critical_trials = function(cleaned_data){
+  data.critical = cleaned_data %>% 
+    filter(type=="critical") %>% 
+    select(submission_id, id, type, question, response, expected, id1, id2,
+           selected_pic, RT) %>% 
+    mutate(expectation = case_when(expected == "none" ~ FALSE,
+                                   TRUE ~ TRUE)) %>%
+    group_by(id1, id2)
+  
+  picA = "A. w/oDistractor-internal"
+  picB = "B. w/oDistractor-external"
+  picC = "C. withDistractor-internal"
+  picD = "D. withDistractor-external"
+  
+  data.critical$picPair = data.critical %>% group_indices()
+  data.critical <- data.critical %>% 
+    mutate(picPair=paste(id1, id2, sep=" & "),
+           picPair.short = case_when(picPair == "if2_unn & if2_unu" ~ picD, 
+                                     picPair == "if1_un & if2_unu" ~ picB,
+                                     picPair == "if1_un & if1_uu" ~ picA,
+                                     picPair == "if2_unn & if1_uu" ~ picC)) %>% 
+    group_by(picPair, question) %>% mutate(n=n()) %>% 
+    mutate(y=case_when(response == "exhaustive" ~ 1, T ~ 0)) %>%
+    group_by(picPair) %>% 
+    mutate(group_id = cur_group_id(), 
+           pics = chartr('1234', 'ABCD', group_id)) %>% 
+    group_by(pics, question) %>%
+    mutate(pics = as.factor(pics), 
+           question = factor(question, levels = c("neutral", "ifp", "willq"))) %>%
+    rename(submissionID = submission_id) %>% 
+    unite("picPair.long", "pics", "picPair", sep=". ") %>% 
+    dplyr::select(-group_id) %>% 
+    rename(picPair = picPair.short) %>% 
+    mutate(picPair = as.factor(picPair), picPair.short = picPair) %>%
+    separate(picPair.short, into=c("picPair.short", "tmp"), sep="\\.") %>% 
+    mutate(picPair.short = as.factor(picPair.short)) %>% 
+    dplyr::select(-tmp)
+  return(data.critical)
+}
+
+
+# Bootstrapping -----------------------------------------------------------
+get_mean_selection_rate <- function(dat, indices) {
+  return(dat[indices, ] %>% pull(response) %>% mean())
+}
+# returns the statistics calculated for each of N bootstrap samples
+get_bootstrap_statistics = function(dat, response, N=1000) {
+  bootstrap_samples <- group_map(dat, function(df, df.group) {
+    bootstrap = boot(df, statistic = get_mean_selection_rate, R=N)$t
+    samples = tibble(rate=bootstrap[,1]) 
+    samples.ordered = left_join(df.group, samples, by=character()) %>%
+      arrange(rate) %>% rowid_to_column("idx")
+    return(samples.ordered)
+   }) %>% bind_rows() %>% add_column(response = response)
+  return(bootstrap_samples)
+}
+get_bootstrap_cis = function(bootstrap_samples, N=1000) {
+  CI_bounds = c(low = ceiling(0.025 * N), up = ceiling(0.975 * N))
+  bootstrapped_cis <- bootstrap_samples %>% 
+    group_by(across(c(-idx, -rate, -response))) %>% 
+    mutate(CI = case_when(idx == CI_bounds[["low"]] ~ "ci.low",
+                          idx == CI_bounds[["up"]] ~ "ci.up")) %>% 
+    filter(!is.na(CI)) %>% dplyr::select(-idx) %>% 
+    pivot_wider(names_from = "CI", values_from = "rate")
+  return(bootstrapped_cis)
+}
+
+# returns bootstrapped confidence intervals for each stimulus + qud and
+# also only for each qud, with empirical mean rate 
+bootstrap_rate_exhaustive = function(df.boot, N=1000){
+  df = df.boot %>% group_by(picPair, picPair.long, picPair.short, question)
+  samples.picPair_qud = get_bootstrap_statistics(df, "exhaustive", N)
+  cis.picPair_qud <- left_join(
+    get_bootstrap_cis(samples.picPair_qud, N),
+    # empriical rate
+    df %>% summarize(rate_exhaustive = mean(response))
+  )
+  samples.qud = samples.picPair_qud %>% group_by(question) %>% 
+    arrange(question, rate) %>% mutate(idx = row_number()) %>% 
+    dplyr::select(-picPair.long, -picPair.short, -picPair)
+  cis.qud = left_join(
+    get_bootstrap_cis(samples.qud, N=samples.qud$idx %>% max()),
+    df.boot %>% group_by(question) %>% summarize(rate_exhaustive=mean(response))
+  )
+  return(list(rate_qud_picPair = cis.picPair_qud, rate_qud = cis.qud))
+}
